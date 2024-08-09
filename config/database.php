8351b68a2c30047f0b76bbdfd5708cae9078d1fb
Chan Hung <tangchanhungit@gmail.com>
2024-08-09 10:58:01 +0700

    update db_host

--- config/database.php
+++ config/database.php
@@ -42,7 +42,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'db_freshfood'),
             'username' => env('DB_USERNAME', 'root'),
